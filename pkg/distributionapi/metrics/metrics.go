e59d0f20d936e395776edb331f7455d23b8b0c0e
laixintao <laixintaoo@gmail.com>
2025-10-27 15:31:05 +0800

    metrics: fix db operation labels

--- pkg/distributionapi/metrics/metrics.go
+++ pkg/distributionapi/metrics/metrics.go
@@ -18,12 +18,12 @@ var (
 	DBQueryTotal = prometheus.NewCounterVec(prometheus.CounterOpts{
 		Name: "piccolo_db_query_total",
 		Help: "Total number of database queries.",
-	}, []string{"operation", "table"})
+	}, []string{"table", "operation"})
 
 	DBQueryDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
 		Name: "piccolo_db_query_duration_seconds",
 		Help: "Duration of database queries in seconds.",
-	}, []string{"operation", "table"})
+	}, []string{"table", "operation"})
 
 	FindKeyHolderCountBucket = prometheus.NewHistogram(
 		prometheus.HistogramOpts{
